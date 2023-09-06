98a4bca5b23ead15cbc79187fc3c48dad79a597b
miguel-gon <gonzalezmiguel211@gmail.com>
2023-09-06 18:26:15 -0400

    TextBoxWithHeading initial and Creations WIP

---
+++ src/Components/TextBoxWithImage/TextBoxWithImage.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {string} text
+ * @param {string} imgPath
+ * @param {string} imgPosition ('top','right','bottom','left')
+ * @returns
+ */
+
+const TextBoxWithImage = ({
+	text = '',
+	imgPath = '../../assets/loona-logo.png',
+	imgPosition = 'top',
+}) => {
+	return (
+		<div className={`textBoxWithImage textBoxWithImage-${imgPosition}`}>
+			<img
+				src={imgPath}
+				className={`textBoxWithImage__image`}
+			/>
+			<div className='textBoxWithImage__headerContainer'>
+				<h2 className='textBoxWithImage__header'>{text}</h2>
+			</div>
+		</div>
+	);
+};
+
+export default TextBoxWithImage;
--- src/Views/Creations/Creations.js
+++ src/Views/Creations/Creations.js
@@ -1,9 +1,26 @@
 import HeaderBox from '../../Components/HeaderBox/HeaderBox';
+import TextBoxWithImage from '../../Components/TextBoxWithImage/TextBoxWithImage';
 
 const Creations = () => {
 	return (
 		<div className='creations'>
-			<HeaderBox header={'Creations'} />
+			<HeaderBox header={'Portfolio'} />
+			<p className='creations__introParagraph'>
+				Click below to find my crochet creations, digital art, and the
+				jewellery-making I dabble in.
+			</p>
+			<TextBoxWithImage
+				text={'Crochet'}
+				imgPosition='left'
+			/>
+			<TextBoxWithImage
+				text={'Art'}
+				imgPosition='right'
+			/>
+			<TextBoxWithImage
+				text={'Jewellry'}
+				imgPosition='left'
+			/>
 		</div>
 	);
 };
